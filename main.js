0fd2478a7f174fef34378a5fe7325f9ce5fc0414
Vign <113303279+7Vign7@users.noreply.github.com>
2024-03-08 23:48:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,28 +2,32 @@ let notesArr = []
 
 function getAddBtn(text){
     let buttonAdd = document.createElement("button")
+    buttonAdd.classList.add("btn")
     buttonAdd.textContent= text
     return buttonAdd
 }
 
-function getCard(){
-    let card = {
-        title: "Название" ,
-        img: "https://sun9-7.userapi.com/impg/Udczrbie-PEE1DY2hip8afa6XsBEpTYWvHQDoQ/y8T8sUXWs_4.jpg?size=744x749&quality=95&sign=96ef2cf8dd1b0c8abb34e7c38abb3ca9&type=album",
-        desc: "Описание карточки"
-    }
+function getCard(card, index){
 
     let cardElement = document.createElement("li")
     let cardImg = document.createElement("img")
     let cardTitle = document.createElement("h2")
     let cardDesc = document.createElement("p")
+    let cardBtn = document.createElement("div")
     let cardImportantBtn = document.createElement("button")
     let cardRemoveBtn = document.createElement("button")
 
     cardElement.classList.add("card")
+
+    if (card.done === true){
+        cardImportantBtn.textContent = "Не важное"
+        cardElement.classList.add("card__coll")
+    }
+
     cardImg.classList.add("card__img")
     cardTitle.classList.add("card__title")
     cardDesc.classList.add("card__desc")
+    cardBtn.classList.add("card__box_btn")
     cardImportantBtn.classList.add("card__import")
     cardRemoveBtn.classList.add("card__remove")
 
@@ -34,7 +38,25 @@ function getCard(){
     cardImportantBtn.textContent ="Важное"
     cardRemoveBtn.textContent ="Удалить"
 
-    cardElement.append(cardImg, cardTitle, cardDesc, cardImportantBtn, cardRemoveBtn)
+    cardRemoveBtn.onclick = function (){
+        notesArr.splice(index, 1)
+        render(notesArr)
+    }
+    cardImportantBtn.onclick = function (){
+        if(cardElement.classList.contains("card__coll")===false){
+            cardImportantBtn.textContent = "Не важное"
+            cardElement.classList.add("card__coll")
+            card.done = true
+        }
+        else{
+            cardImportantBtn.textContent = "Важное"
+            cardElement.classList.remove("card__coll")
+            card.done = false
+        }
+    }
+
+    cardBtn.append(cardImportantBtn, cardRemoveBtn)
+    cardElement.append(cardImg, cardTitle, cardDesc, cardBtn)
 
     return cardElement
 }
@@ -49,15 +71,17 @@ let list = getList()
 
 let addBtn = getAddBtn("Добавить новую замтку ")
 
+
 addBtn.onclick = function (){
     let titleValue = prompt("Введите название заметки")
     let imgValue = prompt("Введите путь заметки")
-    let decsValue = prompt("Введите описание заметки")
+    let descValue = prompt("Введите описание заметки")
 
     let newNoteObj ={
         title: titleValue,
         img: imgValue,
-        decs: decsValue
+        desc: descValue,
+        done: false
     }
 
     notesArr.push(newNoteObj)
@@ -66,9 +90,13 @@ addBtn.onclick = function (){
 }
 
 function render(arrNotes){
+    list.innerHTML=""
     for(let i = 0; i<arrNotes.length; i++){
-        console.log(arrNotes[i])
+        let newCard = getCard(arrNotes[i], i)
+        list.append(newCard)
     }
 }
+
+render(notesArr)
 
 document.body.append(addBtn, list)
